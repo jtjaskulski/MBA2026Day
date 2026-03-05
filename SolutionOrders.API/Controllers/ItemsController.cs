9c0fe332f01ebaf275ec5f949057a4949f99dcde
JakubJaskulki <jtjaskulski@outlook.com>
2026-03-05 17:20:02 +0100

    feat(Lecture3): Add project cleanup script and initial ItemsController

---
+++ SolutionOrders.API/Controllers/ItemsController.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolutionOrders.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        [HttpGet]
+        //[ProducesResponseType(typeof(IEnumerable<ItemDto>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllItems()
+        {
+            
+            throw new NotImplementedException();
+            // // Tworzymy Query
+            // var query = new GetAllItemsQuery();
+            //
+            // // Wysyłamy do MediatR
+            // return Ok(await mediator.Send(query));
+        }
+
+        /// <summary>
+        /// Pobiera produkt po ID
+        /// </summary>
+        [HttpGet("{id}")]
+        // [ProducesResponseType(typeof(ItemDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            
+            throw new NotImplementedException();
+            // var query = new GetItemByIdQuery(id);
+            // var result = await mediator.Send(query);
+            //
+            // if (result == null)
+            // {
+            //     return NotFound(new { message = $"Produkt o ID {id} nie został znaleziony" });
+            // }
+            //
+            // return Ok(result);
+        }
+
+        /// <summary>
+        /// Tworzy nowy produkt
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Create()//[FromBody] CreateItemCommand command)
+        {
+            // var itemId = await mediator.Send(command);
+            //
+            // // HTTP 201 Created z Location header
+            // return CreatedAtAction(nameof(GetById), new { id = itemId },
+            //     new { id = itemId, message = "Produkt został utworzony" }
+            // );
+            
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Aktualizuje produkt
+        /// </summary>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Update()//int id, [FromBody] UpdateItemCommand command)
+        {
+            // if (id != command.IdItem)
+            // {
+            //     return BadRequest(new { message = "ID w URL różni się od ID w body" });
+            // }
+            //
+            // try
+            // {
+            //     await mediator.Send(command);
+            //     return NoContent(); // HTTP 204 - sukces bez body
+            // }
+            // catch (KeyNotFoundException ex)
+            // {
+            //     return NotFound(new { message = ex.Message });
+            // }
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Usuwa produkt (soft delete)
+        /// </summary>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            // var command = new DeleteItemCommand(id);
+            //
+            // try
+            // {
+            //     await mediator.Send(command);
+            //     return NoContent();
+            // }
+            // catch (KeyNotFoundException ex)
+            // {
+            //     return NotFound(new { message = ex.Message });
+            // }
+            throw new NotImplementedException();
+        }
+    }
+}
