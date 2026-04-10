521dc67b433deb50b22e8e5f87fe18b10885a1a4
JakubJaskulki <jtjaskulski@outlook.com>
2026-04-10 16:43:46 +0200

    feat(navigation): integrate RootNavigator and add DetailsScreen component

--- SolutionOrdersMobile/App.tsx
+++ SolutionOrdersMobile/App.tsx
@@ -11,78 +11,16 @@ import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import RootNavigator from './src/navigation/RootNavigator';
 import Greeting from './src/components/Greeting';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
-  return (
-    // <SafeAreaProvider>
-    //   <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-    //   <AppContent />
-    // </SafeAreaProvider>
-    //  <View style={styles.container}>
-    //   <Text style={styles.title}>Hello React Native!</Text>
-    //   <Text style={styles.subtitle}>with TypeScript 🚀</Text>
-    // </View>
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView style={styles.container}>
-        <Greeting name="Emilia" isVip={true} />
-        <Greeting name="Błażej Leśniak" isVip={true} />
-        <Greeting name="Albi Lubila Mayamwene" age={22} isVip={true} />
-        <Greeting name="Paweł Tokarczyk" isVip={true} />
-        <Greeting name="Adrian Kurcharski" isVip={true} />
-        <Greeting name="Klimkiewicz" isVip={true} />
-        <Greeting name="Jakub Jaskulski" age={33} isVip={false} />
-      </ScrollView>
-    </SafeAreaProvider>
+  return ( 
+      <RootNavigator /> 
   );
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-// });
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f5f5f5',
-  },
-});
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#f5f5f5',
-//   },
-//   title: {
-//     fontSize: 24,
-//     fontWeight: 'bold',
-//     color: '#333',
-//   },
-//   subtitle: {
-//     fontSize: 16,
-//     color: '#666',
-//     marginTop: 8,
-//   },
-// });
 
 export default App;
